5006582f886044c52e912837ff07424caaa6c88b
Nanipolabattula <nanipolabattula@gmail.com>
2025-05-26 17:15:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
+const port = process.env.PORT || 3000;  // Use Azure's port or fallback to 3000 locally
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello from CI/CD!\n');
 });
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
+
 
